569d96f2a8aab51f288713b1c3e045be2bd38ea8
Ballester <pedballester@gmail.com>
2015-12-30 16:33:25 -0200

    tr file

--- deep_dive_example.py
+++ deep_dive_example.py
@@ -6,8 +6,8 @@ sess = tf.InteractiveSession()
 
 import trainer_reader as tr
 
-x = tf.placeholder("float", shape=[None, 784])
-y_ = tf.placeholder("float", shape=[None, 10])
+x = tf.placeholder("float", shape=[None, 101568])
+y_ = tf.placeholder("float", shape=[None, 101568])
 
 global_step = tf.Variable(0, trainable=False)
 images, labels = tr.read_from_file('/home/ballester/Documents/DeepDive/Photo3D/train.tfrecords')
@@ -23,29 +23,29 @@ def bias_variable(shape):
 def conv2d(x, W):
   return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-def max_pool_2x2(x):
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
+# def max_pool_2x2(x):
+#   return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+#                         strides=[1, 2, 2, 1], padding='SAME')
 
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
+W_conv1 = weight_variable([38, 121, 3, 1])
+b_conv1 = bias_variable([38])
 
-x_image = tf.reshape(x, [-1,28,28,1])
+x_image = tf.reshape(x, [-1,184,184,3])
 
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+# h_pool1 = max_pool_2x2(h_conv1)
 
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
+W_conv2 = weight_variable([121, 38, 3, 1])
+b_conv2 = bias_variable([38])
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
+h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2) + b_conv2)
+# h_pool2 = max_pool_2x2(h_conv2)
 
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
-b_fc1 = bias_variable([1024])
+# W_fc1 = weight_variable([7 * 7 * 64, 1024])
+# b_fc1 = bias_variable([1024])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+# h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+# h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 keep_prob = tf.placeholder("float")
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
