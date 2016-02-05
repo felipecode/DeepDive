9e9b13c398287edb623e780c694fa41f244463d9
joel gaya <joelfelipe94@gmail.com>
2016-02-05 17:18:10 -0200

    Updated the simulator to make groundtruths

---
+++ Simulator/simulateTurbidImageGT.m
@@ -0,0 +1,20 @@
+function T = simulateTurbidImageGT(input,c,distance,minpixel)
+
+    % Set distance of the scene, for our experiment is always 0.58
+    
+
+     TR = calculateGTImageFixD(double(input(:,:,1))/255,c(1),distance,minpixel); 
+
+     
+     
+     TG = calculateGTImageFixD(double(input(:,:,2))/255,c(2),distance,minpixel); 
+
+     TB = calculateGTImageFixD(double(input(:,:,3))/255,c(3),distance,minpixel); 
+%     
+%     
+     T(:,:,1) = TR;
+     T(:,:,2) = TG;
+     T(:,:,3) = TB;
+     
+
+end
