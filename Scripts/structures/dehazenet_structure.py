afd717b7baf2fa42695bc5894e4f8f72a1cfb366
Joel Gaya <joelfelipe94@github.com>
2016-04-06 19:41:25 -0300

    adicionando codigo do dehanet

---
+++ Scripts/structures/dehazenet_structure.py
@@ -0,0 +1,70 @@
+"""
+Returns the last tensor of the network's structure.
+Input is tensorflow class and an input placeholder.  """
+def create_structure(tf, x, input_size,dropout):
+ 
+  """Deep dive libs"""
+  from deep_dive import DeepDive
+
+  """Our little piece of network for ultimate underwater deconvolution and domination of the sea-world"""
+  deep_dive = DeepDive()
+
+  """Reshaping images"""
+
+  x_image =x
+
+    # INPUT ARE PATCHES 16x16x3 Color
+ 
+  W_conv1_1 = deep_dive.weight_variable_scaling([5,5,3,4], name='W_conv1_1')
+  b_conv1_1 = deep_dive.bias_variable([4])
+  W_conv1_2 = deep_dive.weight_variable_scaling([5,5,3,4], name='W_conv1_2')
+  b_conv1_2 = deep_dive.bias_variable([4])
+  W_conv1_3 = deep_dive.weight_variable_scaling([5,5,3,4], name='W_conv1_3')
+  b_conv1_3 = deep_dive.bias_variable([4])
+  W_conv1_4 = deep_dive.weight_variable_scaling([5,5,3,4], name='W_conv1_4')
+  b_conv1_4 = deep_dive.bias_variable([4])
+  """print  conv1"""
+  conv1_1=deep_dive.conv2d(x_image, W_conv1_1,strides=[1, 1, 1, 1], padding='VALID') + b_conv1_1
+  conv1_2=deep_dive.conv2d(x_image, W_conv1_2,strides=[1, 1, 1, 1], padding='VALID') + b_conv1_2
+  conv1_3=deep_dive.conv2d(x_image, W_conv1_3,strides=[1, 1, 1, 1], padding='VALID') + b_conv1_3
+  conv1_4=deep_dive.conv2d(x_image, W_conv1_4,strides=[1, 1, 1, 1], padding='VALID') + b_conv1_4
+
+  #pool1 = tf.nn.max_pool(conv1, ksize=[1, 1, 1, 4], strides=[1, 1, 1, 4], padding='SAME', name='first_Pool')
+  pool1_1 = tf.reduce_max(conv1_1, reduction_indices=[3], keep_dims=True, name='first_Pool_1')
+  pool1_2 = tf.reduce_max(conv1_2, reduction_indices=[3], keep_dims=True, name='first_Pool_2')
+  pool1_3 = tf.reduce_max(conv1_3, reduction_indices=[3], keep_dims=True, name='first_Pool_3')
+  pool1_4 = tf.reduce_max(conv1_4, reduction_indices=[3], keep_dims=True, name='first_Pool_4')
+  pool1=tf.concat(3, [pool1_1,pool1_2,pool1_3,pool1_4])
+  print pool1
+  """Inception 1 """
+  """ TODO: , padding with variable size """
+  W_incep1_3_3 = deep_dive.weight_variable_scaling([3,3,4,16], name='W_incep1_3_3')
+  W_incep1_5_5 = deep_dive.weight_variable_scaling([5,5,4,16], name='W_incep1_5_5')
+  W_incep1_7_7 = deep_dive.weight_variable_scaling([7,7,4,16], name='W_incep1_7_7')
+
+  b_incep1_3_3 = deep_dive.bias_variable([16])
+  b_incep1_5_5 = deep_dive.bias_variable([16])
+  b_incep1_7_7 = deep_dive.bias_variable([16])
+
+  incep1_3_3 = deep_dive.conv2d(pool1, W_incep1_3_3, padding='SAME') + b_incep1_3_3
+  incep1_5_5 = deep_dive.conv2d(pool1, W_incep1_5_5, padding='SAME') + b_incep1_5_5
+  incep1_7_7 = deep_dive.conv2d(pool1, W_incep1_7_7, padding='SAME') + b_incep1_7_7
+
+  incep1 = tf.concat(3, [incep1_3_3, incep1_5_5, incep1_7_7])
+
+  print  incep1
+
+  pool2 = tf.nn.max_pool(incep1, ksize=[1, 7, 7, 1], strides=[1, 7, 7, 1], padding='VALID', name='second_Pool')
+
+  print pool2
+
+  W_conv2 = deep_dive.weight_variable_scaling([4,4,48,1], name='W_conv2')
+  b_conv2 = deep_dive.bias_variable([1])
+  one_constant = tf.constant(1)
+  conv2 = tf.minimum(tf.to_float(one_constant),tf.nn.relu(deep_dive.conv2d(pool2, W_conv2, padding='SAME') + b_conv2, name="second_relu"))
+
+  print conv2
+
+  # TODO : TRY MULTISCALE DEPATCHFICATION . INTERESTING STUFF FOR NEURAL NETWORKS 
+
+  return conv2,conv2
