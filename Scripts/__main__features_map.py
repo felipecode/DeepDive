171b0c7ba3d2c71b494ab5d1be808039860274b9
Joel Gaya <joelfelipe94@github.com>
2016-04-19 16:16:52 -0300

    modificação do arquivo config

--- Scripts/__main__features_map.py
+++ Scripts/__main__features_map.py
@@ -26,11 +26,11 @@ import time
 from ssim_tf import ssim_tf
 
 """Verifying options integrity"""
-
-if restore not in (True, False):
+config= configMain()
+if config.restore not in (True, False):
   raise Exception('Wrong restore option. (True or False)')
 
-dataset = DataSetManager(path, val_path, pathGroundTruth, out_path, input_size, output_size, proportions)
+dataset = DataSetManager(config.training_path, config.validation_path, config.training_path_ground_truth,config.validation_path_ground_truth, config.input_size, config.output_size, config.proportions)
 global_step = tf.Variable(0, trainable=False, name="global_step")
 
 
@@ -68,7 +68,7 @@ dout4 = tf.placeholder("float")
 # sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=True))
 sess = tf.InteractiveSession()
 
-last_layer, l2_reg, feature_maps = create_structure(tf, x,input_size,[dout1,dout2,dout3,dout4])
+last_layer, l2_reg, feature_maps = create_structure(tf, x,config.input_size,[dout1,dout2,dout3,dout4])
 
 #y_image = tf.reshape(y_, [-1, output_size[0], output_size[1], output_size[2]])
 y_image = y_
@@ -90,7 +90,7 @@ loss_function_ssim = ssim_tf(tf,y_image,last_layer)
 
 
 
-train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss_function)
+train_step = tf.train.AdamOptimizer(config.learning_rate).minimize(loss_function)
 #tf.add_to_collection('losses', loss_validation)
 
 
@@ -151,7 +151,7 @@ sess.run(tf.initialize_all_variables())
 
 
 
-summary_writer = tf.train.SummaryWriter(summary_path,
+summary_writer = tf.train.SummaryWriter(config.summary_path,
                                             graph_def=sess.graph_def)
 
 # """Open tensorboard"""
@@ -159,12 +159,12 @@ summary_writer = tf.train.SummaryWriter(summary_path,
   
 """Load a previous model if restore is set to True"""
 
-if not os.path.exists(models_path):
-  os.mkdir(models_path)
-ckpt = tf.train.get_checkpoint_state(models_path)
+if not os.path.exists(config.models_path):
+  os.mkdir(config.models_path)
+ckpt = tf.train.get_checkpoint_state(config.models_path)
 
 print ckpt
-if restore:
+if config.restore:
   if ckpt.model_checkpoint_path:
     print 'Restoring from ', ckpt.model_checkpoint_path  
     saver.restore(sess,ckpt.model_checkpoint_path)
@@ -172,7 +172,7 @@ else:
   ckpt = 0
 
 
-print 'Logging into ' + summary_path
+print 'Logging into ' + config.summary_path
 
 
 
@@ -191,10 +191,10 @@ if ckpt:
 else:
   initialIteration = 1
 
-for i in range(initialIteration, n_epochs*n_images_dataset):
+for i in range(initialIteration, config.n_epochs*dataset.getNImagesDataset()):
 
   
-  epoch_number = 1.0+ (float(i)*float(batch_size))/float(n_images_dataset)
+  epoch_number = 1.0+ (float(i)*float(config.batch_size))/float(dataset.getNImagesDataset())
 
 
   
@@ -202,7 +202,7 @@ for i in range(initialIteration, n_epochs*n_images_dataset):
   #if i%(n_images/batch_size) == 1:
   #  dataset = manager.read_data_sets2(n_images=n_images,n_images_validation=n_images_validation)
   
-  batch = dataset.train.next_batch(batch_size)
+  batch = dataset.train.next_batch(config.batch_size)
   
 
 
@@ -213,7 +213,7 @@ for i in range(initialIteration, n_epochs*n_images_dataset):
     #   saver.save(sess, models_path + 'model.ckpt', global_step=i + int(ckpt.model_checkpoint_path.split('-')[1]))
     #   print 'Model saved.'
     # else:
-    saver.save(sess, models_path + 'model.ckpt', global_step=i)
+    saver.save(sess, config.models_path + 'model.ckpt', global_step=i)
     print 'Model saved.'
 
 
@@ -223,21 +223,22 @@ for i in range(initialIteration, n_epochs*n_images_dataset):
   #    x:batch[0], y_: batch[1]})
   """Run training and write the summaries"""
   #train_step.run(feed_dict={x: batch[0], y_: batch[1]})
-  
-  sess.run(train_step, feed_dict={x: batch[0], y_: batch[1],dout1:dropout[0],dout2:dropout[1],dout3:dropout[2],dout4:dropout[3]})
+  feedDict={x: batch[0], y_: batch[1] , dout1:1,dout2:1,dout3:1,dout4:1}
+
+  sess.run(train_step, feed_dict={x: batch[0], y_: batch[1],dout1:config.dropout[0],dout2:config.dropout[1],dout3:config.dropout[2],dout4:config.dropout[3]})
   
 
 
   duration = time.time() - start_time
 
   if i%20 == 0:
-    num_examples_per_step = batch_size 
+    num_examples_per_step = config.batch_size 
     examples_per_sec = num_examples_per_step / duration
-    train_accuracy = sess.run(loss_function, feed_dict={x: batch[0], y_: batch[1],dout1:1,dout2:1,dout3:1,dout4:1})
+    train_accuracy = sess.run(loss_function, feed_dict=feedDict)
     if  train_accuracy < lowest_error:
       lowest_error = train_accuracy
       lowest_iter = i
-    print("Epoch %f step %d, images used %d, loss %g, lowest_error %g on %d,examples per second %f"%(epoch_number, i, i*batch_size, train_accuracy, lowest_error, lowest_iter,examples_per_sec))
+    print("Epoch %f step %d, images used %d, loss %g, lowest_error %g on %d,examples per second %f"%(epoch_number, i, i*config.batch_size, train_accuracy, lowest_error, lowest_iter,examples_per_sec))
   
 
 
@@ -246,16 +247,16 @@ for i in range(initialIteration, n_epochs*n_images_dataset):
   #else:
   """ Writing summary, not at every iterations """
   if i%20 == 0:
-    batch_val = dataset.validation.next_batch(batch_size)
-    summary_str = sess.run(summary_op, feed_dict={x: batch[0], y_: batch[1],dout1:1,dout2:1,dout3:1,dout4:1})
-    summary_str_val,result= sess.run([val,last_layer], feed_dict={x: batch_val[0], y_: batch_val[1],dout1:1,dout2:1,dout3:1,dout4:1})
+    batch_val = dataset.validation.next_batch(config.batch_size)
+    summary_str = sess.run(summary_op, feed_dict=feedDict)
+    summary_str_val,result= sess.run([val,last_layer], feed_dict=feedDict)
     summary_writer.add_summary(summary_str,i)
 
     """ Check here the weights """
 
 
     result = Image.fromarray((result[0,:,:,:]*255).astype(np.uint8))
-    result.save(out_path + str(str(i)+ '.jpg'))
+    result.save(config.validation_path_ground_truth + str(str(i)+ '.jpg'))
     summary_writer.add_summary(summary_str_val,i)
   
 
--- Scripts/config.py
+++ Scripts/config.py
@@ -41,36 +41,37 @@ dropout.append(1)
 dropout.append(1) """
 
 
-""" Tranining Configuration for Dehazenet """
+class configMain:
+	def __init__(self):
+		self.learning_rate = 5*1e-5
+		self.batch_size = 4
+		self.n_epochs = 40   # the number of epochs that we are going to run
+		self.training_path = '../datasets/dataset2_2F/Training/'
+		self.training_path_ground_truth = '../datasets/dataset2_2F/GroundTruth'
+		self.validation_path = '../datasets/dataset2_2F/Validation/'
+		self.summary_path = '/tmp/dataset4_124'
+		self.validation_path_ground_truth = '../datasets/dataset4_1/ValidationGroundTruth/'
+		self.models_path = 'models/deepdivearch0.2s_d2.2F_mar_21/'
+		self.input_size = (512, 512, 3)
+		self.output_size = (512, 512, 3)
+		self.ground_truth_size = (512, 512, 3)
+		self.restore = False
+		self.proportions = [1, 1, 1, 1, 1, 1,1]
+		self.dropout = [1,1,1,1]
 
-learning_rate = 5*1e-5
-init_std_dev = 0.1
-l2_reg_w     = 1e-4
-batch_size   = 5
-max_kernel_size = 15
-
-#patch_size = 128
-
-array_path = '../datasets/dataset4_1/Arrays/'
-input_size = (16, 16, 3)
-output_size = (16, 16)
-path = '../datasets/dataset4_1/Training'
-pathGroundTruth = '../datasets/dataset4_1/Transmission'
-val_path = '../datasets/dataset4_1/Validation'
-#n_images = 200			#Number of images to be generated at each time in memory
-n_images_dataset = 16025	#Number of images in the whole dataset
-#n_images_validation = 20
-n_images_validation_dataset = 1994
-
-#proportions = [0.2, 0.2, 0.2, 0.1, 0.1, 0.1, 0.05, 0.05]	#Proportion of each folder to be loaded
-proportions = [1, 1, 1, 1, 1, 1,1]
-n_epochs = 40   # the number of epochs that we are going to run
-
-models_path = 'models/'
-# path = '/home/nautec/Framework-UFOG/CPP/ancuti4.png'
-summary_path = '/tmp/dataset4_120'
-out_path = '../datasets/dataset4_1/ValidationTransmission/'
-
-restore = False
-
-num_gpus = 1
+class configDehazeNet:
+	def __init__(self):
+		self.learning_rate = 5*1e-5
+		self.batch_size = 5
+		self.n_epochs = 40   # the number of epochs that we are going to run
+		self.training_path = '../datasets/dataset4_1/Training'
+		self.training_path_ground_truth = '../datasets/dataset4_1/Transmission'
+		self.validation_path = '../datasets/dataset4_1/Validation'
+		self.summary_path = '/tmp/dataset4_124'
+		self.validation_path_ground_truth = '../datasets/dataset4_1/ValidationTransmission/'
+		self.models_path = 'models/'
+		self.input_size = (16, 16, 3)
+		self.output_size = (16, 16)
+		self.ground_truth_size = (16,16)
+		self.restore = False
+		self.proportions = [1, 1, 1, 1, 1, 1,1]
