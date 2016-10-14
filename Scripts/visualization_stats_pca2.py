6079943f67eacefaf3179dceb9def821d1f62a12
Eglen Protas <you@example.com>
2016-10-14 14:23:16 -0300

    mudanças na visualização por pca

--- Scripts/visualization_stats_pca2.py
+++ Scripts/visualization_stats_pca2.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.mlab import PCA
+from mpl_toolkits.mplot3d import Axes3D
 import time
 import math
 from config import *
@@ -70,27 +72,53 @@ plt.legend()
 if len(dados)>0:
 	dkeys=dados[0].keys()
 
-offset=1.0/len(dados)
-
 for ft_key, ft_ind in zip(config.features_list, xrange(len(config.features_list))):
 	if ft_key in dados[0]:
-		plt.figure(ft_ind+2)
-		plt.grid(True)
-		plt.suptitle(ft_key)
-		axes = plt.gca()
-		actvs=[]
+		data_length=[]
+		n_channels=np.array(dados[i][ft_key]).shape[1]
+		actvs=np.empty([0,n_channels],dtype=np.float32)
+
+		#means=np.empty([len(dados),n_channels],dtype=np.float32)
+		#variances=np.empty([len(dados),n_channels],dtype=np.float32)
+
+		fig = plt.figure() # Make a plotting figure
+		ax = Axes3D(fig) # use the plotting figure to create a Axis3D object.	
+
+		#min_vals=np.amin(np.array(pca_r.Y[:,0:3]),axis=0)
+		#max_vals=np.amax(np.array(pca_r.Y[:,0:3]),axis=0)
+ 		
+
+		# make simple, bare axis lines through space:
+		#xAxisLine = ((min_vals[0], max_vals[0]), (0, 0), (0,0)) # 2 points make the x-axis line at the data extrema along x-axis 
+		#ax.plot(xAxisLine[0], xAxisLine[1], xAxisLine[2], 'black') # make a red line for the x-axis.
+		#yAxisLine = ((0, 0), (min_vals[1], max_vals[1]), (0,0)) # 2 points make the y-axis line at the data extrema along y-axis
+		#ax.plot(yAxisLine[0], yAxisLine[1], yAxisLine[2], 'black') # make a red line for the y-axis.
+		#zAxisLine = ((0, 0), (0,0), (min_vals[2], max_vals[2])) # 2 points make the z-axis line at the data extrema along z-axis
+		#ax.plot(zAxisLine[0], zAxisLine[1], zAxisLine[2], 'black') # make a red line for the z-axis.
+
+		# label the axes
+		ax.set_xlabel("PC1") 
+		ax.set_ylabel("PC2")
+		ax.set_zlabel("PC3")
+		ax.set_title(ft_key)
+
 		for i in xrange(len(dados)):
-			actvs.append(np.array(dados[i][ft_key]))
-		n_channels=actvs[0].shape[1]
-		batch_number = range(0,actvs[0].shape[0])
-		num_plots=min(10,n_channels)
-		for ch in xrange(n_channels):			
-			plt.gca().set_color_cycle(color_cycle)
-			for i in xrange(len(actvs)):
-				if ch==0:
-					plt.plot(np.full(actvs[i].shape[0],ch+i*offset,dtype=float), actvs[i][:,ch], '.', label=names[i])
-				else:
-					plt.plot(np.full(actvs[i].shape[0],ch+i*offset,dtype=float), actvs[i][:,ch], '.')
-		plt.legend(numpoints=1)						
+			actvs=np.array(dados[i][ft_key])
+			pca_r=PCA(actvs)
+			x = []
+			y = []
+			z = []
+			for item in pca_r.Y:
+ 				x.append(item[0])
+ 				y.append(item[1])
+ 				z.append(item[2])
+			pltData = [x,y,z]
+			ax.scatter(pltData[0], pltData[1], pltData[2], s=5, marker='.', color=color_cycle[i], edgecolors='none') # make a scatter plotfrom the data
+			ax.plot([], [], 'o', color=color_cycle[i], label=names[i])#gambiarra pra legenda aparecer		
+
+		plt.legend(numpoints=1)
 		
+		
+		
+											
 plt.show()
