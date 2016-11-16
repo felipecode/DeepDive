09c17a4565db5b6550fd859afe7f37aa1ffa56f5
Eglen Protas <you@example.com>
2016-11-16 19:47:09 -0200

    simulador em teste

---
+++ Scripts/simulator.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import numpy as np
+
+
+def acquireProperties(turbidity_patches):
+	l=1.06      #Schechner,2006
+	T=1       #Transmission coefficient at the water surface - from Processing of field spectroradiometric data - EARSeL
+	I0=1.0
+	turbidity_patches=tf.maximum(turbidity_patches,0.001)
+	turbidity_patches=tf.truediv(turbidity_patches,(l*T*I0))
+	turbidity_patches=-tf.log(turbidity_patches)
+	c=tf.reduce_mean(turbidity_patches, reduction_indices=(1,2))
+	patch_max=tf.reduce_max(turbidity_patches, reduction_indices=(1,2))
+	binf=l*T*I0*tf.exp(tf.mul(-c,patch_max))
+	return c,binf
+
+def applyTurbidity(images, depths, c, binf, ranges):
+	max_range_dev=ranges[0]/2.0
+	ranges+=tf.random_uniform(ranges.get_shape(), minval=-max_range_dev, maxval=max_range_dev) #adicionando variacao no ranges
+	ranges=tf.expand_dims(tf.expand_dims(ranges,1),1)
+	depths=tf.mul(depths, ranges)
+	depths=tf.expand_dims(depths,3)
+	trans=tf.concat(3, [tf.concat(3, [depths, depths]),depths]) #nao sei se tem um jeito melhor de fazer isso
+	c=tf.expand_dims(tf.expand_dims(c,1),1)#outra gambiarra, talves tenha um jeito melhor de fazer isso
+	binf=tf.expand_dims(tf.expand_dims(binf,1),1)#outra gambiarra, talves tenha um jeito melhor de fazer isso
+	trans=tf.exp(-tf.mul(trans,c))
+	Ed=tf.mul(images,trans)
+	Eb=binf-tf.mul(trans,binf)
+	return Ed+Eb
+
+#    J=patch;
+#    trans=ones([size(patch)]);
+#    for i=1:3
+#        trans(:,:,i)=exp(-distance*c(i));
+#    end
+#    Ed = J.*trans;                                      % Direct component
+#    for i=1:3
+#        Eb(:,:,i) = (Binf(i) - Binf(i).*trans(:,:,i));  % Backscattering component
+#    end
+#    turbidPatch = Ed + Eb;   
---
+++ Scripts/teste_simulator.py
@@ -0,0 +1,156 @@
+"""Deep dive libs"""
+from input_data_levelDB import DataSetManager
+from config import *
+from utils import *
+from simulator import *
+#from features_optimization import optimize_feature
+#from loss_network import *
+
+"""Structure"""
+import sys
+sys.path.append('structures')
+sys.path.append('utils')
+#from perceptual_loss_network import create_structure
+
+"""Core libs"""
+import tensorflow as tf
+import numpy as np
+
+"""Visualization libs"""
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+
+"""Python libs"""
+import os
+from optparse import OptionParser
+from PIL import Image
+import subprocess
+import time
+from ssim_tf import ssim_tf
+from scipy import misc
+import glob
+
+import json
+
+
+"""Verifying options integrity"""
+config = configMain()
+
+input_size=(224,224)
+turbidity_size=(128,128)
+batch_size=20
+t_batch_size=4
+range_max=5.0
+
+if config.restore not in (True, False):
+  raise Exception('Wrong restore option. (True or False)')
+if config.save_features_to_disk not in (True, False):
+  raise Exception('Wrong save_features_to_disk option. (True or False)')
+if config.save_json_summary not in (True, False):
+  raise Exception('Wrong save_json_summary option. (True or False)')
+if config.use_tensorboard not in (True, False):
+  raise Exception('Wrong use_tensorboard option. (True or False)')
+
+sim_input_path='../datasets/simteste/images'
+sim_depth_path='../datasets/simteste/depths'
+turbidity_path='../datasets/simteste/TurbidityV3'
+
+inputs = np.empty((batch_size,)+input_size+(3,))
+depths = np.empty((batch_size,)+input_size)
+turbidities=np.empty((t_batch_size,)+turbidity_size+(3,))
+
+input_names=glob.glob(sim_input_path + "/*.png")
+depth_names=glob.glob(sim_depth_path + "/*.png")
+t_imgs_names=glob.glob(turbidity_path + "/*.png")
+
+#print t_imgs_names[2]
+#print input_names[2]
+#print depth_names[2]
+
+
+for i in xrange(batch_size):
+	in_image = Image.open(input_names[i]).convert('RGB')
+	in_image = in_image.resize(input_size, Image.ANTIALIAS)
+	in_image = np.asarray(in_image)
+	in_image = in_image.astype(np.float32)
+	inputs[i] = np.multiply(in_image, 1.0 / 255.0)
+
+	d_image = Image.open(depth_names[i]).convert('L')
+	d_image = d_image.resize(input_size, Image.ANTIALIAS)
+	d_image = np.asarray(d_image)
+	d_image = d_image.astype(np.float32)
+	depths[i] = np.multiply(d_image, 10.0 / 255.0)
+
+
+for i in xrange(t_batch_size):
+	t_image = Image.open(t_imgs_names[i]).convert('RGB')
+	t_image = t_image.resize(turbidity_size, Image.ANTIALIAS)
+	t_image = np.asarray(t_image)
+	t_image = t_image.astype(np.float32)
+	turbidities[i] = np.multiply(t_image, 1.0 / 255.0)
+
+""" Creating section"""
+#x = tf.placeholder("float",(config.batch_size,)+config.input_size, name="input_image")
+#y_ = tf.placeholder("float",(config.batch_size,)+config.output_size, name="output_image")
+#lr = tf.placeholder("float", name = "learning_rate")
+#training = tf.placeholder(tf.bool, name="training")
+
+sess = tf.InteractiveSession()
+
+
+
+
+tf_turbidity=tf.placeholder("float",turbidities.shape, name="turbidity")
+properties=acquireProperties(tf_turbidity)
+
+c, binf=sess.run(properties, feed_dict={tf_turbidity: turbidities})
+
+#colocando os vetores no tamanho do batch, nao sei se tem um jeito melhor de fazer isso
+c_old=c
+c=np.empty((batch_size,c_old.shape[1]))
+for i in xrange(batch_size):
+	c[i]=c_old[i%len(c_old)]
+
+binf_old=binf
+binf=np.empty((batch_size,binf_old.shape[1]))
+for i in xrange(batch_size):
+	binf[i]=binf_old[i%len(binf_old)]
+
+
+range_step=range_max/t_batch_size
+range_values=np.empty(t_batch_size)
+for i in xrange(t_batch_size):
+	range_values[i]=(i+1)*range_step
+
+#parte fixa do range
+range_array=np.empty(batch_size)
+for i in xrange(batch_size):
+	range_array[i]=range_values[(i/(batch_size/t_batch_size))%t_batch_size]
+
+tf_images=tf.placeholder("float",inputs.shape, name="images")
+tf_depths=tf.placeholder("float",depths.shape, name="depths")
+tf_range=tf.placeholder("float",range_array.shape, name="ranges")
+tf_c=tf.placeholder("float",c.shape, name="c")
+tf_binf=tf.placeholder("float",binf.shape, name="binf")
+
+feedDict={tf_images: inputs, tf_depths: depths, tf_range: range_array, tf_c: c, tf_binf: binf}
+
+turbid_images=applyTurbidity(tf_images, tf_depths, tf_c, tf_binf, tf_range)
+
+start_time =time.time()
+result=sess.run(turbid_images, feed_dict=feedDict)
+duration=time.time()-start_time
+
+print duration
+
+img=result[2]
+img=(img-img.min())
+img*=(255/img.max())
+img=img.astype(np.uint8)
+img = Image.fromarray(img)
+img.save("resultado.png")
+
+#print np.amax(result[2,:,:,0]), np.amin(result[2,:,:,0])
+#print np.amax(result[2,:,:,1]), np.amin(result[2,:,:,1])
+#print np.amax(result[2,:,:,2]), np.amin(result[2,:,:,2])
