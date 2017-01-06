68d2f6373a3b4aa1aa9cd8e76f2713e027e4baaa
Eglen Protas <you@example.com>
2017-01-05 23:32:37 -0200

    adicionando variaveis de bias que faltavam

--- Scripts/structures/residual_elu.py
+++ Scripts/structures/residual_elu.py
@@ -11,14 +11,17 @@ def residual(tf, x,base_name,training=True):
   x_image=x
   W_conv = deep_dive.weight_variable_scaling([3,3,64,64], name='W_conv1'+base_name)
   #conv = tf.nn.elu(tf.contrib.layers.batch_norm(deep_dive.conv2d(x_image, W_conv,strides=[1, 1, 1, 1], padding='SAME'),center=True,updates_collections=None,scale=True,is_training=training))
-  conv = tf.nn.elu(deep_dive.conv2d(x_image, W_conv,strides=[1, 1, 1, 1], padding='SAME'))
+  b_conv=deep_dive.bias_variable([64])
+
+  conv = tf.nn.elu(deep_dive.conv2d(x_image, W_conv,strides=[1, 1, 1, 1], padding='SAME')+B_conv)
   print conv
   features["conv"+base_name]=[conv,None]
   histograms["conv"+base_name]=W_conv
 
   W_conv2 = deep_dive.weight_variable_scaling([3,3,64,64], name='W_conv2'+base_name)
+  b_conv2=deep_dive.bias_variable([64])
   #conv2 = tf.contrib.layers.batch_norm(deep_dive.conv2d(x_image, W_conv2,strides=[1, 1, 1, 1], padding='SAME'),center=True,updates_collections=None,scale=True,is_training=training)
-  conv2 = deep_dive.conv2d(x_image, W_conv2,strides=[1, 1, 1, 1], padding='SAME')
+  conv2 = deep_dive.conv2d(x_image, W_conv2,strides=[1, 1, 1, 1], padding='SAME')+B_conv2
   print conv2
   features["conv2"+base_name]=[conv2,None]
   histograms["conv2"+base_name]=W_conv2
