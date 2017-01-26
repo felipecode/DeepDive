fb57d78408f31a605bdbc6ae1b70afcef9a29d5c
Eglen Protas <you@example.com>
2017-01-26 16:02:27 -0200

    atualizando save_max_activations_to_disk para o novo padrao de entrada

--- Scripts/utils.py
+++ Scripts/utils.py
@@ -90,14 +90,13 @@ def save_feature_maps_to_disk(feature_maps, weights, deconv, feature_names,path)
 
 
 def save_max_activations_to_disk(max_activation, feature_names,path):
-
 	for actv, key in zip(max_activation, feature_names):
-		for i in xrange(actv[0].shape[3]):
-			actv_img=actv[0][:,:,:,i]
+		for i in xrange(actv[0].shape[4]):
+			actv_img=actv[0][0,:,:,:,i]
 			actv_img = (actv_img-actv_img.min())
 			actv_img*=(255/(actv_img.max()+0.0001))
 			max_actv_img=actv_img.astype(np.uint8)
-			input_img=actv[1][:,:,i]
+			input_img=actv[1][0,:,:,i]
 			input_img = (input_img-input_img.min())
 			input_img*=(255/(input_img.max()+0.0001))
 			max_actv_input_img=input_img.astype(np.uint8) 
