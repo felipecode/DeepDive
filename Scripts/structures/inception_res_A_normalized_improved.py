199a939c56d2a9bb86e22bb058d1f661d08d90e2
Joel Gaya <joelfelipe94@github.com>
2017-02-16 11:15:20 -0200

    faltou o layer A e B

---
+++ Scripts/structures/inception_res_A_normalized_improved.py
@@ -0,0 +1,58 @@
+
+def inception_res_A(tf, x,base_name,training=True):
+
+
+  from deep_dive import DeepDive
+  deep_dive = DeepDive()
+  features={}
+  scalars={}
+  histograms={}
+
+  x_image=x
+  W_A_conv1 = deep_dive.weight_variable_scaling([1,1,32,12], name='W_A_conv1'+base_name)
+  A_conv1 = tf.contrib.layers.batch_norm(deep_dive.conv2d(x_image, W_A_conv1,strides=[1, 1, 1, 1], padding='SAME'),center=True,updates_collections=None,scale=True,is_training=training)
+  print A_conv1
+  features["A_conv1"+base_name]=[A_conv1,None]
+  histograms["W_A_conv1"+base_name]=W_A_conv1
+
+  W_A_conv2 = deep_dive.weight_variable_scaling([1,1,32,12], name='W_A_conv2'+base_name)
+  A_conv2 = tf.contrib.layers.batch_norm(deep_dive.conv2d(x_image, W_A_conv2,strides=[1, 1, 1, 1], padding='SAME'),center=True,updates_collections=None,scale=True,is_training=training)
+  print A_conv2
+  features["A_conv2"+base_name]=[A_conv2,None]
+  histograms["W_A_conv2"+base_name]=W_A_conv2
+
+  W_A_conv3 = deep_dive.weight_variable_scaling([3,3,12,12], name='W_A_conv3'+base_name)
+  A_conv3 = tf.contrib.layers.batch_norm(deep_dive.conv2d(A_conv2, W_A_conv3,strides=[1, 1, 1, 1], padding='SAME'),center=True,updates_collections=None,scale=True,is_training=training)
+  print A_conv3
+  features["A_conv3"+base_name]=[A_conv3,None]
+  histograms["W_A_conv3"+base_name]=W_A_conv3
+
+  W_A_conv4 = deep_dive.weight_variable_scaling([1,1,32,16], name='W_A_conv4'+base_name)
+  A_conv4 = tf.contrib.layers.batch_norm(deep_dive.conv2d(x_image, W_A_conv4,strides=[1, 1, 1, 1], padding='SAME'),center=True,updates_collections=None,scale=True,is_training=training)
+  features["A_conv4"+base_name]=[A_conv4,None]
+  histograms["W_A_conv4"+base_name]=W_A_conv4
+
+  W_A_conv5 = deep_dive.weight_variable_scaling([3,3,16,24], name='W_A_conv5'+base_name)
+  A_conv5 = tf.contrib.layers.batch_norm(deep_dive.conv2d(A_conv4, W_A_conv5,strides=[1, 1, 1, 1], padding='SAME'),center=True,updates_collections=None,scale=True,is_training=training)
+  print A_conv5
+  features["A_conv5"+base_name]=[A_conv5,None]
+  histograms["W_A_conv5"+base_name]=W_A_conv5
+
+  W_A_conv6 = deep_dive.weight_variable_scaling([3,3,24,32], name='W_A_conv6'+base_name)
+  A_conv6 = tf.contrib.layers.batch_norm(deep_dive.conv2d(A_conv5, W_A_conv6,strides=[1, 1, 1, 1], padding='SAME'),center=True,updates_collections=None,scale=True,is_training=training)
+  print A_conv6
+  features["A_conv6"+base_name]=[A_conv6,None]
+  histograms["W_A_conv6"+base_name]=W_A_conv6
+
+  A_concat = tf.concat(3, [A_conv1,A_conv3,A_conv6])
+
+  W_A_conv7 = deep_dive.weight_variable_scaling([1,1,56,32], name='W_A_conv7'+base_name)
+  A_conv7 = tf.contrib.layers.batch_norm(deep_dive.conv2d(A_concat, W_A_conv7,strides=[1, 1, 1, 1], padding='SAME'),center=True,updates_collections=None,scale=True,is_training=training)
+  print A_conv7
+  features["A_conv7"+base_name]=[A_conv7,None]
+  histograms["W_A_conv7"+base_name]=W_A_conv7
+
+  A_relu = tf.nn.relu(A_conv7+ x_image)
+  
+  features["A_relu"+base_name]=[A_relu,None]
+  return A_relu,features,histograms
---
+++ Scripts/structures/inception_res_B_normalized_improved.py
@@ -0,0 +1,47 @@
+
+def inception_res_B(tf, x,base_name,training=True):
+
+  from deep_dive import DeepDive
+  deep_dive = DeepDive()
+  features={}
+  scalars={}
+  histograms={}
+
+  x_image=x
+  x_image = tf.contrib.layers.batch_norm(x_image,center=True,updates_collections=None,scale=True,is_training=training)
+  W_B_conv1 = deep_dive.weight_variable_scaling([1,1,32,48], name='W_B_conv1'+base_name)
+  B_conv1 = tf.contrib.layers.batch_norm(deep_dive.conv2d(x_image, W_B_conv1,strides=[1, 1, 1, 1], padding='SAME'),center=True,updates_collections=None,scale=True,is_training=training)
+  print B_conv1
+  features["B_conv1"+base_name] = [B_conv1,None]
+  histograms["W_B_conv1"+base_name]=W_B_conv1
+
+  W_B_conv2 = deep_dive.weight_variable_scaling([1,1,32,48], name='W_B_conv2'+base_name)
+  B_conv2 = tf.contrib.layers.batch_norm(deep_dive.conv2d(x_image, W_B_conv1,strides=[1, 1, 1, 1], padding='SAME'),center=True,updates_collections=None,scale=True,is_training=training)
+  print B_conv2
+  features["B_conv2"+base_name] = [B_conv2,None]
+  histograms["W_B_conv2"+base_name]=W_B_conv2
+
+  W_B_conv3 = deep_dive.weight_variable_scaling([1,3,48,56], name='W_B_conv3'+base_name)
+  B_conv3 = tf.contrib.layers.batch_norm(deep_dive.conv2d(B_conv2, W_B_conv3,strides=[1, 1, 1, 1], padding='SAME'),center=True,updates_collections=None,scale=True,is_training=training)
+  print B_conv3
+  features["B_conv3"+base_name] = [B_conv3,None]
+  histograms["W_B_conv3"+base_name]=W_B_conv3
+
+  W_B_conv4 = deep_dive.weight_variable_scaling([3,1,56,64], name='W_B_conv4'+base_name)
+  B_conv4 = tf.contrib.layers.batch_norm(deep_dive.conv2d(B_conv3, W_B_conv4,strides=[1, 1, 1, 1], padding='SAME'),center=True,updates_collections=None,scale=True,is_training=training)
+  print B_conv4
+  features["B_conv4"+base_name] = [B_conv4,None]
+  histograms["W_B_conv4"+base_name]=W_B_conv4
+
+  B_concat = tf.concat(3, [B_conv1,B_conv4])
+
+  W_B_conv5 = deep_dive.weight_variable_scaling([1,1,112,32], name='W_B_conv5'+base_name)
+  B_conv5 = tf.contrib.layers.batch_norm(deep_dive.conv2d(B_concat, W_B_conv5,strides=[1, 1, 1, 1], padding='SAME'),center=True,updates_collections=None,scale=True,is_training=training)
+  print B_conv5
+  features["B_conv5"+base_name]=[B_conv5,None]
+  histograms["W_B_conv5"+base_name]=W_B_conv5
+
+  B_relu = tf.nn.relu(B_conv5+ x_image)
+  features["B_relu"+base_name]=[B_relu,None]
+  return B_relu,features,histograms
+
